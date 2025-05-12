2fa13af7b4e5ac83e3018a3b40f34e5f5f32a2e3
anshu-chaurasia <anshu.chaurasia@evident.capital>
2025-05-12 17:39:42 +0530

    changed viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Make sure base is set to '/' for Netlify deployments
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
